3b299d5db44f8c91f522d6f342d96d88bd34c206
Alexei Barnes <alexei.barnes@aveva.com>
2021-02-21 18:06:50 +0000

    Created basic pings service in an RPC server to test clients

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -1,33 +1,8 @@
-use pings_rpc::pings_service_server;
-pub use pings_rpc::pings_service_server::PingsServiceServer;
+pub use pings_rpc::pings_service_server::{PingsService, PingsServiceServer};
 pub use pings_rpc::{Ping, Pings, PingsRequest};
-use std::time::{Duration, SystemTime};
-pub use tonic::{transport::Server, Request, Response, Status};
 
 pub mod pings_rpc {
     tonic::include_proto!("pings");
-}
-
-#[derive(Debug, Default)]
-pub struct PingsService {}
-
-#[tonic::async_trait]
-impl pings_service_server::PingsService for PingsService {
-    async fn get_pings(&self, request: Request<PingsRequest>) -> Result<Response<Pings>, Status> {
-        println!("Got a request: {:?}", request);
-
-        let reply = Pings {
-            ip: "0.0.0.0".into(),
-            domain: "localhost".into(),
-            pings: vec![Ping {
-                time: Some(SystemTime::now().into()),
-                duration: Some(Duration::from_millis(8).into()),
-            }]
-            .into(),
-        };
-
-        Ok(Response::new(reply))
-    }
 }
 
 #[cfg(test)]
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,28 @@
-use pot_rpc::{PingsService, PingsServiceServer, Server};
+use pot_rpc::{self, PingsServiceServer, PingsRequest, Pings, Ping};
+use std::time::{Duration, SystemTime};
+use tonic::{transport::Server, Request, Response, Status};
+
+#[derive(Debug, Default)]
+pub struct PingsService {}
+
+#[tonic::async_trait]
+impl pot_rpc::PingsService for PingsService {
+    async fn get_pings(&self, request: Request<PingsRequest>) -> Result<Response<Pings>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = Pings {
+            ip: "0.0.0.0".into(),
+            domain: "localhost".into(),
+            pings: vec![Ping {
+                time: Some(SystemTime::now().into()),
+                duration: Some(Duration::from_millis(8).into()),
+            }]
+            .into(),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
